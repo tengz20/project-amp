ed21ef0d5acd35df7982d572cc28542a27949c39
tengz20 <101984391+tengz20@users.noreply.github.com>
2024-03-29 14:47:48 -0400

    Update amp_script.js

--- amp_script.js
+++ amp_script.js
@@ -52,7 +52,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 		},
 
 		base_url : {//Where are your images at?
-			image : 'https://tengz20.github.io/project-amp/images/'
+			image : 'https://tengz20.github.io/project-amp/images'
 		}
 	});
 });
